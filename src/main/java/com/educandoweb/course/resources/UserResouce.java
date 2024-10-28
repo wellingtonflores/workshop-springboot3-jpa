f96af3a6f0101725a0c247402c59ce3fe5748b40
wellingtonflores <wellingtonluiscardoso@hotmail.com>
2024-10-28 11:38:18 -0300

    User update

--- src/main/java/com/educandoweb/course/resources/UserResouce.java
+++ src/main/java/com/educandoweb/course/resources/UserResouce.java
@@ -36,4 +36,16 @@ public class UserResouce {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).body(obj);
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj){
+        obj = userService.update(id, obj);
+        return ResponseEntity.ok().body(obj);
+    }
 }
